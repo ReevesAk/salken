8ca11c6d9524e1b4e9238fe9ccd7bdb8ff451098
ReevesAk <reeves.akwa@gmail.com>
2023-02-09 14:21:50 +0100

    create cart page

--- salken/urls.py
+++ salken/urls.py
@@ -24,7 +24,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name='home'),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('cart.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
