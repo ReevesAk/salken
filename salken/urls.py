25e06932fb141bba0826eabc628d026bc796b015
ReevesAk <reeves.akwa@gmail.com>
2023-02-09 14:02:19 +0100

    some cleanup, redicrect page in product image click

--- salken/urls.py
+++ salken/urls.py
@@ -19,9 +19,12 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name='home'),
-    path('store', include('store.urls'))
+    path('store/', include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/views.py
+++ store/views.py
@@ -12,7 +12,7 @@ def store(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True )
+        products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:     
         products = Product.objects.all().filter(is_available=True)
@@ -21,9 +21,11 @@ def store(request, category_slug=None):
     context = {
         'products': products,
     }
-    return render(request, template_name='store/store.html', context=context)
+    return render(request=request, template_name='store.html', context=context)
 
 
+# product_detail handles the logic of the details displayed about a product as
+#  well as how it is displayed.
 def product_detail(request):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
