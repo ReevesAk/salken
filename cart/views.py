e0d337db14d7508d4edc419071d535caf2b22508
ReevesAk <reeves.akwa@gmail.com>
2023-02-13 23:31:38 +0100

    tax and grand total

--- cart/views.py
+++ cart/views.py
@@ -43,6 +43,8 @@ def cart(request, total=0, quantity=0, cart_item=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (24 * total) / 100
+        grand_total = total + tax
     except ObjectNotExist:
         pass    
 
@@ -50,7 +52,9 @@ def cart(request, total=0, quantity=0, cart_item=None):
     context = {
         'total': total,
         'quantity': quantity,
-        'cart_items': cart_items
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
     }
 
     return render(request=request, template_name='store/cart.html', context=context)
