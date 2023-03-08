040ddf5184ab3d631a8ed7bf4b90d4492cf6f199
ReevesAk <reeves.akwa@gmail.com>
2023-03-08 19:03:59 +0100

    account verifcation and expire link

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,16 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from . forms import RegistrationForm
 from .models import Account
+from django.http import HttpResponse
+
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 # Create your views here.
 
@@ -27,8 +37,20 @@ def register(request):
                                                 password=password)
             user.phone_no = phone_no
             user.save(self)
-            messages.success(request, 'Registration was susscessful')
-            return redirect('register')
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = "Follow the link to activate your account"
+            message = render_to_string('aaccounts/account_verification_email.html', {
+                "user": user,
+                "domain": current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user), 
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm
         
@@ -63,3 +85,22 @@ def logout(request):
     messages.success(request, "You are logged out")
     return redirect("login")
     render(request=request, template_name='accounts/logout.html')       
+
+
+# activate handles the GET response for account verification via email.
+def activate(request, uidb64, token ):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')    
+
